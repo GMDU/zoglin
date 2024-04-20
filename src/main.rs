f5252f5c308b6c3b81c05625fb80f726982c5929
GearsDatapacks <surya@seriousinfinitude.com>
2024-04-20 13:22:30 +0100

    feat: Add init option

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod compiler;
 mod lexer;
 mod parser;
 
-use std::fs::read_to_string;
+use std::{
+  fs::{self, read_to_string},
+  path::Path,
+};
 
 use lexer::Lexer;
 
@@ -15,23 +18,60 @@ fn main() {
       Arg::new("file").short('f').default_value("main.zog"),
       Arg::new("output").short('o').default_value("build"),
     ]))
+    .subcommand(Command::new("init").arg(Arg::new("name")))
     .get_matches();
 
   if let Some(matches) = matches.subcommand_matches("build") {
     let file: &String = matches.get_one("file").unwrap();
     let output: &String = matches.get_one("output").unwrap();
     build(file, output);
+  } else if let Some(matches) = matches.subcommand_matches("init") {
+    let name = matches.get_one("name");
+    if let Some(name) = name {
+      init(name);
+    } else {
+      init(&String::new());
+    }
   }
 }
 
 fn build(file: &String, output: &String) {
-    let contents = read_to_string(file).unwrap();
-    let mut lexer = Lexer::new(&contents);
-    let tokens = lexer.tokenise();
-  
-    let mut parser = Parser::new(tokens);
-    let ast = parser.parse();
-  
-    let compiler = Compiler::new(ast);
-    compiler.compile(output);
+  let contents = read_to_string(file).unwrap();
+  let mut lexer = Lexer::new(&contents);
+  let tokens = lexer.tokenise();
+
+  let mut parser = Parser::new(tokens);
+  let ast = parser.parse();
+
+  let compiler = Compiler::new(ast);
+  compiler.compile(output);
+}
+
+const DEFAULT_PROJECT: &str = r#"namespace $name {
+  fn tick() {
+
+  }
+
+  fn load() {
+    tellraw @a "Loaded $name"
+  }
+}
+"#;
+
+fn init(name: &String) {
+  if name == "" {
+    let dir = std::env::current_dir().unwrap();
+    let current_dir = Path::new(&dir).file_name().unwrap().to_str().unwrap();
+    let is_empty = dir.read_dir().unwrap().next().is_none();
+    if !is_empty {
+      println!("No init'ing projects in non-empty directories, naughty naughty!");
+      return;
+    }
+    let contents = DEFAULT_PROJECT.replace("$name", current_dir);
+    fs::write("main.zog", contents).unwrap();
+  } else {
+    fs::create_dir(name).unwrap();
+    let contents = DEFAULT_PROJECT.replace("$name", name);
+    fs::write(name.clone() + "/main.zog", contents).unwrap();
+  }
 }
